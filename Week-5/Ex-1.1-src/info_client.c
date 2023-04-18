8d8bfe16eed97fd0c041fe65bf55b4a6f8e59eb1
vu.bl <builongvu2001@gmail.com>
2023-04-19 02:01:08 +0700

    ADD EX1-1 - ADD program templates - UPDATE READMEs - UPDATE sv_client  & sv_client2: Stu_ID condition

--- Week-5/Ex-1.1-src/info_client.c
+++ Week-5/Ex-1.1-src/info_client.c
@@ -6,17 +6,16 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <time.h>
 #include <unistd.h>
 
-// TESTING MACROS
 #define SRV_ADDR "127.0.0.1"
 #define PORT 9999
-#define MAX_SIZE 2048
+#define MAX_SIZE 256
+#define MAX_DRIVE 10
 
-/** @function: VerifyingAddress() 
+/** @function: VerifyAddress()
 * @brief: Checking IPv4 address
-* @param cmd_address: Input argument for server IPv4 address
+* @param cmd_address: Command line argument for server IPv4 address
 *
 * @return: 0 if valid
 *          1 if invalid.
@@ -36,8 +35,8 @@ int VerifyAddress (const char * cmd_address)
         if(oclen > 3) {
             return 1;
         }
-        for (unsigned int iNum = 0; iNum < oclen; ++iNum) {
-            int tmp = sub_buff[iNum] - '0';     // Number only
+        for (unsigned int iDigit = 0; iDigit < oclen; ++iDigit) {
+            int tmp = sub_buff[iDigit] - '0';     // Number only
             if (tmp < 0 || tmp > 9) {
                 return 1;
             }
@@ -49,14 +48,14 @@ int VerifyAddress (const char * cmd_address)
     return 0;
 }
 
-/** @function: VerifyingPort()
+/** @function: VerifyPort()
 * @brief: Checking port
-* @param cmd_port: Input argument for port
+* @param cmd_port: Command line argument for port
 *
 * @return: Port number if valid.
 *          -1 if invalid       
 **/
-int VerifyPort (char * cmd_port)
+int VerifyPort (const char * cmd_port)
 {
     char buff[10];
     strcpy(buff, cmd_port);
@@ -74,229 +73,133 @@ int VerifyPort (char * cmd_port)
     return port;
 }
 
-/** @function: VerifyName()
- * @brief: Checking NAME.
- * @param buff: Buffer contains input from user command line.
- * 
- * @return: 0 - NAME is valid
- *          -1 - NAME is invalid.
-*/
-int VerifyName (char * name) 
+int VerifyDeviceName (char * device_name)
 {
-    // Verify Name
-    for(unsigned int iChar = 0; iChar < strlen(name); ++iChar) {        
-        if (name[iChar] == ' ') {    // Space and new line condition
+    for (unsigned int iChar = 0; iChar < strlen(device_name); ++iChar) {
+        if (device_name[iChar] == '-') 
             continue;
-        }
-        if ( !isalpha(name[iChar]) ) {    
-            return -1;
-        } 
+        if ( !isalpha(device_name[iChar]) && !isdigit(device_name[iChar]))
+            return 1;
     }
-
+    
     return 0;
 }
 
-/** @funtion: VerifyStuID()
- * @brief: Checking STUDENT_ID 
- * @param cmd_id: Array contains STUDENT_ID string 
- *
- * @return: 0 - STUDENT_ID is valid
- *          -1 - STUDENT_ID is invalid.
-*/
-int VerifyStuID(char * cmd_id)
+int VerifyDriveSize (char * drive_size)
 {
-    for(unsigned int iDigit = 0; iDigit < strlen(cmd_id); ++iDigit) {   
-        if ( !isdigit(cmd_id[iDigit]) )     // Number only
-            return -1; 
-    }
-    return 0;
-}
+    char check[3] = "GT";
+    char * ptr = strpbrk(drive_size, check);
+    char buff[8];
 
-/** @funtion: VerifyDoB()
- * @brief: Checking DATE_OF_BIRTH
- * @param cmd_id: Array contains DATE_OF_BIRTH string 
- *
- * @return: 0 - DATE_OF_BIRTH is valid
- *          -1 - DATE_OF_BIRTH is invalid.
-*/
-int VerifyDoB(char * cmd_DoB)
-{
-    char DoB[20];
-    strcpy(DoB, cmd_DoB);
-
-    time_t tick = time(NULL);
-    struct tm t_info = *localtime(&tick);
-    int cur_year = t_info.tm_year + 1900;   // Get Current Year
-
-    char * token = strtok(DoB, "-");    // Parsing Y, M, D
-    while (token != NULL) {
-        char tmp_token[10];
-
-        if (strlen(token) > 4)      // String length check (4 is max for YYYY)
+    strncpy(buff, drive_size, ptr - drive_size);
+    for (unsigned int iChar = 0; iChar < strlen(buff); ++iChar) {
+        if ( !isdigit(buff[iChar]) )
             return -1;
-        strcpy (tmp_token, token);
-        for (unsigned int iChar = 0; iChar < strlen(tmp_token); ++iChar) {  
-            if ( !isdigit(tmp_token[iChar]) )   // Number only
-                return -1;
-        }  
-
-        token = strtok(NULL, "-");       
     }
 
-    int year, month, day;
-    year = month = day = 0;
-    sscanf(cmd_DoB, "%d %d %d", &year, &month, &day);
-    // Failed conditions
-    if (year > cur_year)            return -1;  // Year
-    if (-month < 1 || -month > 12)  return -1;  // Month
-    if (-day < 1 || -day > 31)      return -1;  // Day
-    if (-month == 2 && -day > 29)   return -1;  // Exception February 
-
-    return 0;
-}
-
-/** @funtion: VerifyGPA()
- * @brief: Checking GPA
- * @param cmd_id: Array contains GPA string 
- *
- * @return: 0 - GPA is valid
- *          -1 - GPA is invalid.
-*/
-int VerifyGPA(char * cmd_GPA)
-{
-    int dot_cnt = 0;    // # of decimal points
-    for (unsigned int iDigit = 0; iDigit < strlen(cmd_GPA); ++iDigit) { 
-        if (dot_cnt > 1) 
-            return -1;
-        if (cmd_GPA[iDigit] == '.') {
-            ++dot_cnt;
+    memset(buff, 0, sizeof(buff));
+    strncpy(buff, ptr, 2);
+    for (unsigned int iChar = 0; iChar < strlen(buff); ++iChar) {
+        if ( buff[iChar] == 'B' || buff[iChar] == 'T' || buff[iChar] == 'G')
             continue;
-        }
-        if ( !isdigit(cmd_GPA[iDigit]) )    // Number only
+        else   
             return -1;
     }
 
-    float gpa = strtof(cmd_GPA, NULL);
-    if (gpa > 4.0)  // GPA Limit
-        return -1;        
     return 0;
 }
 
-int main(int argc, char * argv[])
-{ 
+int main()
+{
     char buff[MAX_SIZE];
-
-    // Verifying command
-    if (argc != 3) {
-        printf("[**ERROR]: Incorrect command!\n");
-        printf("[USAGE]: ./sv_client server_IPv4 port\n");
-        exit(1);
-    }
-
-    // Verifying Address
-    if (VerifyAddress(argv[1])) {
-        printf("[**ERROR]: Invalid Address.\n");
-        exit(1);
-    } 
-
-    // Verifying Port
-    int port = VerifyPort(argv[2]);
-    if (port == -1) {
-        printf("[**ERROR]: Invalid Port.\n");
-        exit(1);
-    }
-
     // Initializing socket
     int sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(sock_fd == -1) {
-        perror("[**ERROR] - socket()");
-        exit(1);
-    }
 
     // Updating address info
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(SRV_ADDR);
-    addr.sin_port = htons(port);
+    // inet_aton(argv[1], &addr.sin_addr);
+    addr.sin_port = htons(PORT);
 
-    // Connecting to server
+    // Connecting to the server
     int c_state = connect(sock_fd, (struct sockaddr *)&addr, sizeof(addr));
     if (c_state == -1) {
         perror("[**ERROR] - connect()");
         exit(1);
     }
 
-    // Opening instruction if connect sucessfully
-    FILE * f_handler;
-    f_handler = fopen("sv_instruction2.txt", "r");
-    char ch;
+    puts("Start typing your device's spec");
+    puts("-------------------------------");
+    // Take Input from User
+    char dvc_name[60], disk_size[12];
+    short disk_cnt;
+    unsigned short ptr_pos = 0;
 
-    while((ch = fgetc(f_handler)) != EOF) {
-        putchar(ch);
-    }
-    putchar('\n');
-    fclose(f_handler);
-    
-
-    // Get input from user
-    char name[50], stu_id[15], dob[15], gpa[5];
-
-    memset(buff, 0, MAX_SIZE);
     while (1) {
-        printf("Ho Ten: ");
-        fgets(name, sizeof(name), stdin);
-        name[strlen(name) - 1] = 0;
-        if (VerifyName(name)) {
-            printf("[**ERROR]: Invalid Name.\n");
-            continue;
+        printf("Device's Name: ");
+        fgets(dvc_name, sizeof(dvc_name), stdin);
+        dvc_name[strlen(dvc_name) - 1] = 0;
+        if (VerifyDeviceName(dvc_name)) {
+           printf("[**ERROR]: Invalid Device Name. You cannot use blank spaces and special characters.\n");
+           continue;
         }
-        strcpy(buff, name);
+        strcpy(buff, dvc_name);
         strcat(buff, " ");
-        break;
-    }
-    
-    while (1) {
-        printf("MSSV: ");
-        fgets(stu_id, sizeof(stu_id), stdin);
-        stu_id[strlen(stu_id) - 1] = 0;
-        if (VerifyStuID(stu_id)) {
-            printf("[**ERROR]: Invalid Student ID\n");
-            continue;
-        }
-        strcat(buff, stu_id);
-        strcat(buff, " ");
-        break;
-    }
-    
-    while (1) {
-        printf("Ngay Sinh: ");
-        fgets(dob, sizeof(dob), stdin);
-        dob[strlen(dob) - 1] = 0;
-        if (VerifyDoB(dob)) {
-            printf("[**ERROR]: Invalid Date of Birth.\n");
-            continue;
-        }
-        strcat(buff, dob);
-        strcat(buff, " ");
+        ptr_pos = strlen(buff);
         break;
     }
 
     while (1) {
-        printf("GPA: ");
-        fgets(gpa, sizeof(gpa), stdin);
-        gpa[strlen(gpa) - 1] = 0;
-        if (VerifyGPA(gpa)) {
-            printf("[**ERROR]: Invalid GPA.\n");
+        printf("Number of Drives: ");
+        scanf("%hd", &disk_cnt);
+        getchar();
+
+        if (disk_cnt > 10 || disk_cnt < 0) {
+            printf("[**ERROR]: Invalid Number of Drives.\n");
             continue;
         }
-        strcat(buff, gpa);
-        strcat(buff, "\n");
+        sprintf(buff + ptr_pos, "%hd", disk_cnt);
+        strcat(buff, " ");
+
+        int input_cnt = 0;
+        while (input_cnt < disk_cnt) {
+            char disk_label;
+            ptr_pos = strlen(buff);
+
+            printf("- Disk Label: ");
+            scanf("%c", &disk_label);
+            getchar();
+
+            if ( !isalpha(disk_label) ) {
+                printf("[**ERROR]: Invalid Disk Label.\n");
+                continue;
+            }
+            sprintf(buff + ptr_pos, "%c", disk_label);
+            ptr_pos = strlen(buff);
+
+            while (1) {
+                printf("- Disk Size: ");
+                fgets(disk_size, sizeof(disk_size), stdin);
+                disk_size[strlen(disk_size) - 1] = 0;
+                if (VerifyDriveSize(disk_size)) {
+                    printf("[*ERROR]: Invalid Disk Size Input.\n");
+                    continue;
+                }
+
+                strcat(buff, disk_size);
+                strcat(buff, " ");
+                break;
+            }
+            puts("----------");
+            input_cnt++;
+        }
         break;
     }
-
+    puts("Package to Server:");
+    puts(buff);
     send(sock_fd, buff, strlen(buff), 0);
-    printf("Sent Successfully!\nClosing connection.\n");
-
-    // Closing connetion
+    
+    // Closing connection
     close(sock_fd);
 }
--- Week-5/Ex-1.1-src/info_server.c
+++ Week-5/Ex-1.1-src/info_server.c
@@ -1,78 +1,22 @@
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <time.h>
 #include <unistd.h>
 
-// TESTING MACROS
+#define SRV_ADDR "127.0.0.1"
 #define PORT 9999
-#define MAX_SIZE 2048
+#define MAX_SIZE 256
 
-/** @function: VerifyPort()
-* @brief: Checking port
-* @param cmd_port: Input argument for port
-*
-* @return: -1 if invalid
-*          Port number if valid.
-**/
-int VerifyPort (const char * cmd_port)
+int main() 
 {
-    char buff[10];
-
-    strcpy(buff, cmd_port);
-    for(unsigned int iBuff = 0; iBuff < strlen(buff); ++iBuff) {
-        int tmp = buff[iBuff] - '0';
-        if (tmp < 0 || tmp > 9) {       // Number only
-            return -1;
-        }
-    }
-    int port = atoi(cmd_port);
-    if(port < 1 || port > 65535) {      // Port range
-        return -1;
-    }
-
-    return port;
-}
-
-/** @function: GetTime()
- * @brief: Get system time, format to YYYY-MM-DD Time
- * @param buff: A character array to save the formated time string.
- * 
- * @return: None
-*/
-void GetTime(char * buff)
-{
-    time_t ticks = time(NULL);
-    printf("%.30s", ctime(&ticks));
-    struct tm * t_info = localtime(&ticks);
-    strftime(buff, 50, "%Y-%m-%d %X", t_info);  // Formatting
-}
-
-int main (int argc, char * argv[])
-{
-    char buff[MAX_SIZE];
-
-    // Verifying command
-    if (argc != 3) {
-        printf("[**ERROR]: Incorrect comamnd!\n");
-        printf("[USAGE]: ./sv_server port logFile\n");
-        exit(1);
-    }
-
-    // Verifying port
-    int port = VerifyPort(argv[1]);
-    if (port == -1) {
-        printf("[**ERROR]: Invalid port!\n");
-        exit(1);
-    }
-
     // Initializing socket
     int sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(sock_fd == -1) {
+    if (sock_fd == -1) {
         perror("[**ERROR] - socket()");
         exit(1);
     }
@@ -80,30 +24,37 @@ int main (int argc, char * argv[])
     // Updating address info
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // inet_addr(SRV_ADDR); 
+    addr.sin_port = htons(PORT);
 
     // Binding socket
-    if( bind(sock_fd, (struct sockaddr *)&addr, sizeof(addr)) ) {
+    if (bind(sock_fd, (struct sockaddr *)&addr, sizeof(addr))) {
         perror("[**ERROR] - bind()");
         exit(1);
     }
 
     // Listening for connection
-    if( listen(sock_fd, 5) ) {
+    if (listen(sock_fd, 5)) {
         perror("[**ERROR] - listen()");
         exit(1);
     }
-    printf("Listening on port %d...\n", port);
+    printf("Listening on port %d...\n", PORT);
 
-    // Accepting request
-    struct sockaddr_in clientAddr;  // Save client Address
-    socklen_t c_addrLen = sizeof(clientAddr);   
+    // Accepting connection
+    // Without Client IP
+    int acpt_client = accept(sock_fd, NULL, NULL);
+    if (acpt_client == -1) {
+        perror("[**ERROR] - accept()");
+        exit(1);
+    }
+    
+    puts("New Client Connected.\n");
 
-    int acpt_client = accept(sock_fd, (struct sockaddr *)&clientAddr, &c_addrLen);
-    printf("Connection from: %s\n\n", inet_ntoa(clientAddr.sin_addr));
+    // Receiving from Client
+    char buff[MAX_SIZE];
+    puts("Received from Client:");
+    puts("----------------------");
 
-    // Receiving from client
     while(1) {
         char sub_buff[20];
         memset(sub_buff, 0, sizeof(sub_buff));      // Clear sub_buff
@@ -115,12 +66,23 @@ int main (int argc, char * argv[])
         }
         if (rcv_byte < MAX_SIZE) 
             buff[rcv_byte] = 0;
-        
-        // Save to file
-        GetTime(sub_buff);                     // Add Client's info
-        printf("Received from Client: %s\n", buff);        
-    }
 
+        char * tmp_string  = strtok(buff, " ");
+        printf("Device Name: %s\n", tmp_string);
+
+        tmp_string = strtok(NULL, " ");
+        if (atoi(tmp_string) == 1) {
+            printf("Client has %s disk.\n", tmp_string);
+        } else {
+            printf("Client has %s disks.\n", tmp_string);
+        }
+
+        while ((tmp_string = strtok(NULL, " ")) != NULL) {
+            memset(sub_buff, 0, sizeof(sub_buff));
+            strncpy(sub_buff, tmp_string, strlen(tmp_string));
+            printf("\t+) %c - %s\n", toupper(sub_buff[0]), sub_buff + 1);
+        }
+    }
 
     // Closing connection
     close(acpt_client);
